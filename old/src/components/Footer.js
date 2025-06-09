c08982f243650c7fa6f196ef9ed0646d5255a43c
Artem Nazarchuk <devnazarchuk@gmail.com>
2025-06-09 23:56:03 +0200

    d

--- old/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer class = "flex justify-center bg-slate-300">
-    <p>&copy; 2024 My Next Project</p>
-  </footer>
-);
-
-export default Footer;
--- old/src/services/api.js
+++
@@ -1,6 +0,0 @@
-export const fetchData = async (endpoint) => {
-    const response = await fetch(endpoint);
-    const data = await response.json();
-    return data;
-  };
-  
